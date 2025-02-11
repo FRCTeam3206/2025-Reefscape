461489eb9fb0c226c2eb3d737282f39b74aa3a10
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2025-02-10 18:35:46 -0600

    Log the wrist.

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,11 +5,13 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.WristConstants;
 
+@Logged
 public class Wrist extends SubsystemBase {
   private boolean m_goalHorizontal = true;
   private SparkMax m_wristMotor = new SparkMax(WristConstants.kCANId, MotorType.kBrushless);
