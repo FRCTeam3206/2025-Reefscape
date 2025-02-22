8ee8e94f84d418eb521579d9391f1ede9784fb78
RoyalT-Wrecks <97846880+RoyalT-Wrecks@users.noreply.github.com>
2025-02-22 11:38:13 -0600

    Intake faster.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -165,7 +165,7 @@ public final class Constants {
     public static final DCMotor kArmMotorType = DCMotor.getNEO(1);
     public static final DCMotor kWheelsMotorType = DCMotor.getNeo550(1);
 
-    public static final double kIntakeSpeed = 0.25;
+    public static final double kIntakeSpeed = 0.5;
     public static final double kExtakeSpeed = -0.5;
 
     public static final double kRetractSpeed = -0.6;
